f175da5c5075f931d952ca30cd42f2d40e3c8493
Francisco-Gatica06 <148362037+Francisco-Gatica06@users.noreply.github.com>
2024-08-21 10:22:02 -0400

    Actualización

--- src/CartasJuego.java
+++ src/CartasJuego.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 public class CartasJuego {
     public static void main(String[] args) {
-        inicializarCartasJuego();
+        menu();
     }
     public static String[][] crearMatrizCartas() {
         String[][] matrizDeCartas = new String [12][2];
         return matrizDeCartas;
     }
-    public static String agregarCartas(String nombreCarta, String puntaje) {
-        crearMatrizCartas()[0][0] = nombreCarta;
-        crearMatrizCartas()[0][1] = puntaje;
+    public static void agregarCartas(String nombreCarta, String puntaje) {
+        for (int i = 0; i < crearMatrizCartas().length; i++) {
+            for (int j = 0; j < crearMatrizCartas()[i].length; j++) {
+                crearMatrizCartas()[i][0] = nombreCarta;
+                crearMatrizCartas()[i][j] = puntaje;
+            }
+        }
     }
     public static void inicializarCartasJuego() {
         agregarCartas("A", "11");
@@ -27,7 +33,32 @@ public class CartasJuego {
     }
     public static void obtenerCartas() {}
     public static void jugar() {}
-    public static void salir() {}
-    public static void menu() {}
+    public static void salir() {
+        System.exit(0);
+    }
+    public static String leerDato() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Escoja una opcion: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        return input;
+    }
+    public static void menu() {
+        while (true) {
+            System.out.println("");
+            System.out.println("1 = Obterner carta");
+            System.out.println("2 = Jugar");
+            System.out.println("3 = Salir");
+            if (leerDato() == "1") {
+                obtenerCartas();
+            } else if (leerDato() == "2" ) {
+                jugar();
+            } else if (leerDato() == "3") {
+                System.out.println("Programa finalizado");
+                salir();
+            }
+
+        }
+    }
 
 }
