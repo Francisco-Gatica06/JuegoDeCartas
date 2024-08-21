8c634e0c270f0464ae4beb2d17aceb0ab81ba915
Francisco-Gatica06 <148362037+Francisco-Gatica06@users.noreply.github.com>
2024-08-21 09:14:21 -0400

    commit inicial

---
+++ src/CartasJuego.java
@@ -0,0 +1,13 @@
+public class CartasJuego {
+    public static void main(String[] args) {
+
+    }
+    public static String crearMatrizCartas() {}
+    public static String agregarCartas(String nombreCarta, String puntaje) {}
+    public static void inicializarCartasJuego() {}
+    public static void obtenerCartas() {}
+    public static void jugar() {}
+    public static void salir() {}
+    public static void menu() {}
+
+}
